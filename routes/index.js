46b9917337572d987be93207361c19b13c4e8189
NicoToff <toffolo01@hotmail.com>
2022-06-16 17:15:37 +0200

    Made a beautiful waiting home page

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,10 @@
 const express = require("express");
 const router = express.Router();
 
+let loading = "10";
+
 router.get("/", function (req, res, next) {
-    res.status(200).render("index");
+    res.status(200).render("index", { loading });
 });
 
 module.exports = router;
