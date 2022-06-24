59ff73133a78c234fb1a9973b247938c47bbffde
NicoToff <toffolo01@hotmail.com>
2022-06-24 15:37:45 +0200

    Added pop science hobby with dynamic link inclusion from files

--- routes/hobbies.js
+++ routes/hobbies.js
@@ -3,10 +3,24 @@ const router = require("express").Router();
 const path = require("path");
 const fs = require("fs").promises;
 const dicePath = path.join(__dirname, "../public/images/hobbies/dice");
+const popSciencePath = path.join(__dirname, "../public/images/hobbies/pop-science");
+const urlSciencePath = path.join(__dirname, "../input/links-to-pop-science.txt");
 
 router.get("/", async function (req, res, next) {
     const dice = await fs.readdir(dicePath);
-    res.status(200).render("hobbies", { dice });
+    const channelsImages = await fs.readdir(popSciencePath);
+    let channelUrls = await fs.readFile(urlSciencePath);
+    channelUrls = channelUrls.toString().split("\r\n");
+    const channels = [];
+    channelsImages.forEach((image, i) => {
+        channels.push({
+            image,
+            path: channelUrls[i],
+        });
+    });
+    console.log(channels);
+
+    res.status(200).render("hobbies", { dice, channels });
 });
 
 module.exports = router;
