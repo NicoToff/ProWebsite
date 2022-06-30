e28d742700499a525d67cb3d8049649e080fd1c0
NicoToff <toffolo01@hotmail.com>
2022-06-30 11:50:26 +0200

    Improved content, added early alarm page, added more text to Skills page

--- app.js
+++ app.js
@@ -1,17 +1,12 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const { db } = require("./db/db");
+const { visitorDatabase } = require("./modules/visitorDatabase");
 
 const indexRouter = require("./routes/index");
 const skillsRouter = require("./routes/skills");
 const contactRouter = require("./routes/contact");
 const projectRouter = require("./routes/projects");
-// const projectHelhaRevisionRouter = require("./routes/projects/helha-revision");
-// const project1Router = require("./routes/projects/project1");
-// const project2Router = require("./routes/projects/project2");
-// const currentProjectsRouter = require("./routes/projects/current");
-// const alarmProjectRouter = require("./routes/projects/alarm");
 const hobbiesRouter = require("./routes/hobbies");
 
 const expressLayouts = require("express-ejs-layouts");
@@ -23,59 +18,12 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.set("layout", "layouts/main");
 app.use(expressLayouts);
-
-/* ------------------- DATABASE MIDDLEWARE --------------------------- */
-app.use(async (req, res, next) => {
-    // #region Creating or Retrieving visitor
-    const visitorIpAddress = req.ip;
-
-    let [[visitor]] = await db
-        .promise()
-        .query(`SELECT * FROM visitor WHERE ipAddress = ?`, [visitorIpAddress]);
-
-    let visitorId;
-    if (visitor == null) {
-        const [result] = await db
-            .promise()
-            .query(`INSERT INTO visitor (ipAddress) VALUES (?)`, [visitorIpAddress]);
-        visitorId = result.insertId;
-    } else {
-        visitorId = visitor.id;
-    }
-    // #endregion
-
-    // #region Creating or Retrieving pageSeen
-    // Checking if visitor has seen the current page or not
-    const url = req.url;
-    let [[pageSeen]] = await db
-        .promise()
-        .query(`SELECT * FROM pageSeen WHERE visitor_id = ? AND url = ?`, [visitorId, url]);
-    let pageSeenId;
-    if (pageSeen == null) {
-        const [result] = await db
-            .promise()
-            .query(`INSERT INTO pageSeen (url,visitor_id) VALUES (?,?)`, [url, visitorId]);
-        pageSeenId = result.insertId;
-    } else {
-        pageSeenId = pageSeen.id;
-    }
-    // #endregion
-
-    // #region Creating new timestamp for the visit
-    const now = new Date(Date.now());
-    await db.promise().query(`INSERT INTO timestamp (value, pageSeen_id) VALUES (?,?)`, [now, pageSeenId]);
-    // #endregion
-    next();
-});
+app.use(visitorDatabase);
 
 app.use("/", indexRouter);
 app.use("/skills", skillsRouter);
 app.use("/contact", contactRouter);
 app.use("/projects", projectRouter);
 app.use("/hobbies", hobbiesRouter);
-// app.use("/project1", project1Router);
-// app.use("/project2", project2Router);
-// app.use("/current", currentProjectsRouter);
-// app.use("/alarm", alarmProjectRouter);
 
 module.exports = { app };
---
+++ modules/visitorDatabase.js
@@ -0,0 +1,50 @@
+const { db } = require("../db/db");
+
+const visitorDatabase = async (req, res, next) => {
+    // #region Creating or Retrieving visitor
+    const visitorIpAddress = req.ip;
+
+    if (visitorIpAddress === "::1") {
+        return next();
+    }
+
+    let [[visitor]] = await db
+        .promise()
+        .query(`SELECT * FROM visitor WHERE ipAddress = ?`, [visitorIpAddress]);
+
+    let visitorId;
+    if (visitor == null) {
+        const [result] = await db
+            .promise()
+            .query(`INSERT INTO visitor (ipAddress) VALUES (?)`, [visitorIpAddress]);
+        visitorId = result.insertId;
+    } else {
+        visitorId = visitor.id;
+    }
+    // #endregion
+
+    // #region Creating or Retrieving pageSeen
+    // Checking if visitor has seen the current page or not
+    const url = req.url;
+    let [[pageSeen]] = await db
+        .promise()
+        .query(`SELECT * FROM pageSeen WHERE visitor_id = ? AND url = ?`, [visitorId, url]);
+    let pageSeenId;
+    if (pageSeen == null) {
+        const [result] = await db
+            .promise()
+            .query(`INSERT INTO pageSeen (url,visitor_id) VALUES (?,?)`, [url, visitorId]);
+        pageSeenId = result.insertId;
+    } else {
+        pageSeenId = pageSeen.id;
+    }
+    // #endregion
+
+    // #region Creating new timestamp for the visit
+    const now = new Date(Date.now());
+    await db.promise().query(`INSERT INTO timestamp (value, pageSeen_id) VALUES (?,?)`, [now, pageSeenId]);
+    // #endregion
+    next();
+};
+
+module.exports = { visitorDatabase };
