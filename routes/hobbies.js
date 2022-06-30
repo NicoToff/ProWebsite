dedb9fd35bc2d97e99103983ade104afad7a5fe9
NicoToff <toffolo01@hotmail.com>
2022-06-30 14:27:29 +0200

    Made input file JSON, instead of TXT

--- routes/hobbies.js
+++ routes/hobbies.js
@@ -4,7 +4,7 @@ const path = require("path");
 const fs = require("fs").promises;
 const dicePath = path.join(__dirname, "../public/images/hobbies/dice");
 const popSciencePath = path.join(__dirname, "../public/images/hobbies/pop-science");
-const urlSciencePath = path.join(__dirname, "../input/links-to-pop-science.txt");
+const urlSciencePath = path.join(__dirname, "../input/links-to-pop-science.json");
 
 router.get("/", async function (req, res, next) {
     // Sending dice images from folder
@@ -12,13 +12,14 @@ router.get("/", async function (req, res, next) {
     // Sending channel images and informations
     const channelsImages = await fs.readdir(popSciencePath); // All images are there
     let channelUrls = await fs.readFile(urlSciencePath); // URLs to channels are stored in this file
-    channelUrls = channelUrls?.toString()?.split(";");
+    channelUrls = JSON.parse(channelUrls?.toString());
+
     const channels = [];
     channelsImages.forEach((image, i) => {
         channels.push({
             image,
-            path: channelUrls[i],
-            name: channelUrls[i]?.split("/")?.pop(), // Gets last element of the URL
+            path: channelUrls[i]?.url,
+            name: channelUrls[i]?.url.split("/")?.pop(), // Gets last element of the URL
         });
     });
 
