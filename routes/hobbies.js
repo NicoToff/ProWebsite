c2f96bac4e460b89b45a921362dfb1ad5d04cde8
NicoToff <toffolo01@hotmail.com>
2022-06-30 12:31:57 +0200

    General visuable improvements and corrected typos

--- routes/hobbies.js
+++ routes/hobbies.js
@@ -1,5 +1,6 @@
 "use strict";
 const router = require("express").Router();
+const { url } = require("inspector");
 const path = require("path");
 const fs = require("fs").promises;
 const dicePath = path.join(__dirname, "../public/images/hobbies/dice");
@@ -7,15 +8,18 @@ const popSciencePath = path.join(__dirname, "../public/images/hobbies/pop-scienc
 const urlSciencePath = path.join(__dirname, "../input/links-to-pop-science.txt");
 
 router.get("/", async function (req, res, next) {
+    // Sending dice images from folder
     const dice = await fs.readdir(dicePath);
-    const channelsImages = await fs.readdir(popSciencePath);
-    let channelUrls = await fs.readFile(urlSciencePath);
+    // Sending channel images and informations
+    const channelsImages = await fs.readdir(popSciencePath); // All images are there
+    let channelUrls = await fs.readFile(urlSciencePath); // URLs to channels are stored in this file
     channelUrls = channelUrls.toString().split("\r\n");
     const channels = [];
     channelsImages.forEach((image, i) => {
         channels.push({
             image,
             path: channelUrls[i],
+            name: channelUrls[i].split("/").pop(), // Gets last element of the URL
         });
     });
 
